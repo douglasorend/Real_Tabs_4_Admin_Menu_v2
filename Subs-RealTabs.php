3e06c787f8a103e08edbf01b68a2674842a4f5d0
douglasorend <37510276+douglasorend@users.noreply.github.com>
2018-10-25 15:45:58 -0500

    v1.0 - April 5th, 2014
    v1.0 - April 5th, 2014
    o Initial release to the public

---
+++ Subs-RealTabs.php
@@ -0,0 +1,77 @@
+<?php
+/**********************************************************************************
+* Subs-RealTabs.php - Function to reorganize Admin menu
+*********************************************************************************
+* This program is distributed in the hope that it is and will be useful, but
+* WITHOUT ANY WARRANTIES; without even any implied warranty of MERCHANTABILITY
+* or FITNESS FOR A PARTICULAR PURPOSE .
+**********************************************************************************/
+
+function RT_Reorganize(&$areas)
+{
+	global $txt;
+
+	// Create a "Security" tab before the "Forum" tab and include "Security and Moderation" settings here:
+	$new = array();
+	foreach ($areas as $needle => $section)
+	{
+		if ($needle == 'layout')
+			$new['security'] = array(
+				'title' => $txt['admin_security'],
+				'permission' => array('admin_forum'),
+				'areas' => array(
+					'securitysettings' => $areas['config']['areas']['securitysettings'],
+				),
+			);
+		$new[$needle] = $section;
+	}
+	$areas = $new;
+	unset($new);
+	unset($areas['config']['areas']['securitysettings']);
+
+	// Move the "httpBL", "Bad Behavior" and "Forum FireWall" settings to this section:
+	if (isset($areas['config']['areas']['forumfirewall']))
+		$areas['security']['areas']['forumfirewall'] = $areas['config']['areas']['forumfirewall'];
+	unset($areas['config']['areas']['forumfirewall']);
+
+	if (isset($areas['config']['areas']['badbehavior']))
+		$areas['security']['areas']['badbehavior'] = $areas['config']['areas']['badbehavior'];
+	unset($areas['config']['areas']['badbehavior']);
+
+	if (isset($areas['members']['areas']['httpBL']))
+		$areas['security']['areas']['httpBL'] = $areas['members']['areas']['httpBL'];
+	unset($areas['members']['areas']['httpBL']);
+
+	// Create "Modifications" tab using "Modifications Settings" area:
+	$areas['mods'] = array(
+		'title' => $txt['admin_mods'],
+		'permission' => array('admin_forum'),
+		'areas' => array(
+			'modsettings' => $areas['config']['areas']['modsettings'],
+		),
+	);
+	unset($areas['config']['areas']['modsettings']);
+
+	// Remove most modifications from "Configuration" tab and put under "Modifications":
+	$haystack = array('corefeatures', 'featuresettings', 'languages', 'serversettings', 'modsettings', 'current_theme', 'theme');
+	foreach ($areas['config']['areas'] as $needle => $area)
+	{
+		if (!in_array($needle, $haystack))
+		{
+			$areas['mods']['areas'][$needle] = $areas['config']['areas'][$needle];
+			unset($areas['config']['areas'][$needle]);
+		}
+	}
+
+	// Create "Package Manager" tab, removing it from "Main" tab:
+	$areas['packages'] = array(
+		'title' => $txt['package'],
+		'permission' => array('admin_forum', 'manage_permissions', 'moderate_forum', 'manage_membergroups', 'manage_bans', 'send_mail', 'edit_news', 'manage_boards', 'manage_smileys', 'manage_attachments'),
+		'areas' => array(
+			'packages' => $areas['forum']['areas']['packages'],
+		),
+	);
+	unset($areas['forum']['areas']['packages']);
+}
+
+?>
---
+++ readme.txt
@@ -0,0 +1,41 @@
+[hr]
+[center][color=red][size=16pt][b]REAL TABS FOR ADMIN/MODERATOR MENU v2.0[/b][/size][/color]
+[url=http://www.simplemachines.org/community/index.php?action=profile;u=253913][b]By Dougiefresh[/b][/url] -> [url=http://custom.simplemachines.org/mods/index.php?mod=3796]Link to Mod[/url]
+[/center]
+[hr]
+
+[color=blue][b][size=12pt][u]Introduction[/u][/size][/b][/color]
+In my opinion, phpBB has a nicer and less confusing layout of the admin menu.  This mod attempts to make the menu closer to phpBB's menu layout without majorly rewriting SMF (also known as forking), as that would be too big a task....
+
+[color=blue][b][size=12pt][u]Admin Alterations[/u][/size][/b][/color]
+~ Each header has been transformed into an actual tab at the top, underneath the link tree.
+~ The sidebar menu has been modified, making all areas of a particular header and all subsections of a particular area, into menu items.
+~ The link tree underneath the description has been removed, as the subsections listed are now part of the sidebar.
+
+[b][u]NEW WITH VERSION 2.0![/u][/b]
+~ "Package Manager" is now in its own tab!
+~ "Configuration" tab has been restored back to unmodified condition, with exception of "Security and Moderations" (see below)
+~ All other modifications to "Configuration" tab were moved into a new "Mods" tab
+~ "Security" tab has been created, using "Security and Moderations" area from "Configurations"
+~ [url=http://custom.simplemachines.org/mods/index.php?mod=2155]httpBL[/url], [url=http://custom.simplemachines.org/mods/index.php?mod=2815]Forum Firewall[/url] and [url=http://custom.simplemachines.org/mods/index.php?mod=2502]Bad Behavior[/url] (if they are installed) have been moved to the "Security" tab
+
+[color=blue][b][size=12pt][u]Compatibility Notes[/u][/size][/b][/color]
+This mod was tested on SMF 2.0.7, but should work on earlier versions of SMF 2.0.x.  SMF 1.x is not and will not be supported.
+
+[color=blue][b][size=12pt][u]Other Notes[/u][/size][/b][/color]
+The reorganization of the Admin menu takes place AFTER the processing of the hooks....
+
+[color=blue][b][size=12pt][u]Changelog[/u][/size][/b][/color]
+[b][u]v2.0 - June 11th, 2014[/u][/b]
+o "Package Manager" is now in its own tab!
+o "Configuration" tab has been restored back to unmodified condition, with exception of "Security and Moderations" (see below)
+o All other modifications to "Configuration" tab were moved into a new "Mods" tab
+o "Security" tab has been created, using "Security and Moderations" area from "Configurations"
+o httpBL, Forum Firewall and Bad Behavior (if they are installed) have been moved to the "Security" tab
+
+[b][u]v1.0 - October 20th, 2013[/u][/b]
+o Initial Release
+
+[hr]
+[url=http://creativecommons.org/licenses/by/3.0][img]http://i.creativecommons.org/l/by/3.0/80x15.png[/img][/url]
+This work is licensed under a [url=http://creativecommons.org/licenses/by/3.0]Creative Commons Attribution 3.0 Unported License[/url]
